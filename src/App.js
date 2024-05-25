625f62f608bbc3dd51d73ed4d6e1dc53fc10ed65
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2024-05-25 23:51:05 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -15,59 +15,75 @@ import Restaurant from "./components/Restaurant";
 import BookRoom from "./components/BookRoom";
 import Contact from "./components/Contact";
 import LanguageSwitcher from "./components/LanguageSwitcher";
-import Footer from "./components/Footer.jsx"; // Import the Footer component
+import Footer from "./components/Footer"; // Import the Footer component
 
 function App() {
     return (
         <Router>
-            <AppBar position="static">
-                <Toolbar sx={{ justifyContent: "center" }}>
-                    <Box sx={{ display: "flex", alignItems: "center" }}>
-                        <Typography
-                            variant="h6"
-                            style={{ flexGrow: 1, marginRight: 45 }}
-                        >
-                            Bonus Apartments
-                        </Typography>
-                        <Button color="inherit" component={Link} to="/">
-                            Home
-                        </Button>
-                        <Button color="inherit" component={Link} to="/rooms">
-                            Rooms
-                        </Button>
-                        <Button
-                            color="inherit"
-                            component={Link}
-                            to="/restaurant"
-                        >
-                            Restaurant
-                        </Button>
-                        <Button
-                            color="inherit"
-                            component={Link}
-                            to="/book-room"
-                        >
-                            Book Room
-                        </Button>
-                        <Button color="inherit" component={Link} to="/contact">
-                            Contact
-                        </Button>
-                        <Box ml={2}>
-                            <LanguageSwitcher />
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    minHeight: "100vh",
+                }}
+            >
+                <AppBar position="static">
+                    <Toolbar sx={{ justifyContent: "center" }}>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                            <Typography
+                                variant="h6"
+                                style={{ flexGrow: 1, marginRight: 45 }}
+                            >
+                                Bonus Apartments
+                            </Typography>
+                            <Button color="inherit" component={Link} to="/">
+                                Home
+                            </Button>
+                            <Button
+                                color="inherit"
+                                component={Link}
+                                to="/rooms"
+                            >
+                                Rooms
+                            </Button>
+                            <Button
+                                color="inherit"
+                                component={Link}
+                                to="/restaurant"
+                            >
+                                Restaurant
+                            </Button>
+                            <Button
+                                color="inherit"
+                                component={Link}
+                                to="/book-room"
+                            >
+                                Book Room
+                            </Button>
+                            <Button
+                                color="inherit"
+                                component={Link}
+                                to="/contact"
+                            >
+                                Contact
+                            </Button>
+                            <Box ml={2}>
+                                <LanguageSwitcher />
+                            </Box>
                         </Box>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-
-            <Container style={{ marginTop: "20px" }}>
-                <Routes>
-                    <Route path="/rooms" element={<Rooms />} />
-                    <Route path="/restaurant" element={<Restaurant />} />
-                    <Route path="/book-room" element={<BookRoom />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </Container>
+                    </Toolbar>
+                </AppBar>
+                <Container style={{ flex: 1, marginTop: "20px" }}>
+                    <Routes>
+                        <Route path="/rooms" element={<Rooms />} />
+                        <Route path="/restaurant" element={<Restaurant />} />
+                        <Route path="/book-room" element={<BookRoom />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </Container>
+                <Footer /> {/* Add the Footer component */}
+            </Box>
         </Router>
     );
 }

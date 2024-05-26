c9134db55278eec50510d9bf2024e8dfc6b4e8e9
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2024-05-26 09:09:19 +0200

    rooms

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,4 +1,3 @@
-// src/components/Rooms.jsx
 import React, { useState } from "react";
 import { Container, Typography, Grid, Box, Button } from "@mui/material";
 import Lightbox from "react-image-lightbox";
@@ -10,36 +9,42 @@ const rooms = [
         description:
             "Studio apartment with big terrace, 4 persons, air conditioning, TV, wi fi internet, kitchen, big terrace, bathroom ",
         folder: "room1",
+        bestPhoto: "/images/room1/one1.jpg", // Add the path to the best photo
     },
     {
         title: "Room 2",
         description:
             "Studio apartment with balcony (for 3 persons) air conditioning, TV, wi fi internet , kitchen, balcony, bathroom",
         folder: "room2",
+        bestPhoto: "/images/room2/two10.jpg",
     },
     {
         title: "Room 3",
         description:
             "Triple room with balcony, air conditioning, TV, wi fi internet, fridge, balcony, bathroom",
         folder: "room3",
+        bestPhoto: "/images/room3/three3.jpg",
     },
     {
         title: "Room 4",
         description:
             "Twin room with balcony, air conditioning, TV, wi fi internet, fridge, balcony, bathroom",
         folder: "room4",
+        bestPhoto: "/images/room4/four1.jpg",
     },
     {
         title: "Room 10",
         description:
             "One-bedroom apartment with balcony for 4 persons, living room, bedroom, air conditioning, TV, wi fi internet , kitchen, balcony, bathroom  ",
         folder: "room10",
+        bestPhoto: "/images/room10/ten1.jpg",
     },
     {
         title: "Room 12",
         description:
             "one-bedroom apartment with terrace (up to 5 persons), living room, bedroom, kitchen, air conditioning, TV, wi fi internet , terrace, bathroom ",
         folder: "room12",
+        bestPhoto: "/images/room12/twelve1.jpg",
     },
 ];
 
@@ -60,9 +65,9 @@ const Rooms = () => {
 
     return (
         <Container>
-            <Grid container spacing={4}>
+            <Grid container spacing={2}>
                 {rooms.map((room, index) => (
-                    <Grid item xs={12} sm={6} md={4} sx={{ mt: 5 }} key={index}>
+                    <Grid item xs={12} sm={6} md={4} sx={{ mt: 3 }} key={index}>
                         <Box
                             border={1}
                             padding={2}
@@ -71,16 +76,33 @@ const Rooms = () => {
                             justifyContent="space-between"
                             height="100%"
                         >
-                            <Box>
-                                <Typography variant="h5" gutterBottom>
+                            <Box sx={{ mb: 0.5 }}>
+                                <Typography variant="h6" gutterBottom>
                                     {room.title}
                                 </Typography>
-                                <Typography variant="body1" paragraph>
+                                <Typography
+                                    variant="body1"
+                                    paragraph
+                                    sx={{ fontSize: "14px" }}
+                                >
                                     {room.description}
                                 </Typography>
                             </Box>
                             <Box
-                                mt={2}
+                                component="img"
+                                src={room.bestPhoto}
+                                alt={room.title}
+                                sx={{
+                                    width: "100%",
+                                    height: "140px", // Fixed height
+                                    objectFit: "cover", // Maintain aspect ratio
+                                    border: "2px solid pink",
+                                    borderRadius: "2px",
+                                    mb: 0.5, // Decreased margin-bottom here
+                                }}
+                            />
+                            <Box
+                                mt={0.5} // Decreased margin-top here
                                 display="flex"
                                 flexDirection="column"
                                 gap={1}
@@ -88,19 +110,31 @@ const Rooms = () => {
                                 <Button
                                     variant="contained"
                                     color="primary"
+                                    sx={{ height: 30 }} // Increased button height for consistency
                                     onClick={() => openLightbox(room.folder)}
                                 >
-                                    View Gallery
+                                    <Typography
+                                        variant="body1"
+                                        sx={{ fontSize: "12px" }}
+                                    >
+                                        View Gallery
+                                    </Typography>
                                 </Button>
                                 <Button
                                     variant="contained"
                                     sx={{
-                                        backgroundColor: "pink",
-                                        color: "purple",
+                                        backgroundColor: "darkGreen",
+                                        color: "white",
+                                        height: 30, // Increased button height for consistency
                                     }}
                                     href="/book-room"
                                 >
-                                    Book Room
+                                    <Typography
+                                        variant="body1"
+                                        sx={{ fontSize: "12px" }}
+                                    >
+                                        Book Room
+                                    </Typography>
                                 </Button>
                             </Box>
                         </Box>

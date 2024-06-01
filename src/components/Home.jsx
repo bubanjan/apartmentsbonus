e2be90a31c5674b0545a9710c9aede84a7c3c07b
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2024-06-01 14:54:54 +0200

    fixed translat. home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,18 +12,47 @@ const Home = () => {
                 {t("welcome")}
             </Typography>
             <Typography variant="body1" paragraph>
-                {t("intro")}
+                {t("intro1")}
             </Typography>
+            <Typography variant="body1" paragraph>
+                {t("intro2")}
+            </Typography>
+            <Typography variant="body1" paragraph>
+                {t("intro3")}
+            </Typography>
+            {/*
+ <Typography variant="body1" paragraph>
+                Apartments Bonus is set in a peaceful residential area of Budva
+                near Slovenian beach, just 300 m from the main pebbly beach.
+            </Typography>
+            <Typography variant="body1" paragraph>
+                There is a restaurant on the ground floor of the property which
+                offers half board, full board, breakfast only, or a-la-cart. A
+                grocery shop can be found right next to the property, while
+                numerous bars and nightclubs can be found just a couple of
+                minutes walk from the Bonus. Local buses stop just 100 meters
+                from Bonus Apartments and drive to any of the 14 nice beaches in
+                Budva (Jaz, Becici, Kamenovo, Sveti Stefan...) for just 1.5 EUR
+                per bus ticket.
+            </Typography>
+            <Typography variant="body1" paragraph>
+                We offer accommodation in first category rooms and apartments
+                with air-conditioning, cable TV, free Wi-Fi internet, kitchen
+                (apartments), fridge (rooms), terrace, and bathroom.
+            </Typography>
+
+    */}
+
             <Box
                 display="flex"
                 justifyContent="center"
                 alignItems="center"
-                sx={{ mt: 0 }}
+                sx={{ mt: 5 }}
             >
                 <img
                     src="/images/general/gen1.jpg"
                     alt="Hotel"
-                    style={{ width: "50%", height: "auto" }}
+                    style={{ width: "50%" }}
                 />
             </Box>
         </Container>
